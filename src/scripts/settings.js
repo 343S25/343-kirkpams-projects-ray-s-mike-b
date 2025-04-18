f4fbd4506ea1c905cc6e71f33d11bea091adc80c
Mike Buckingham <michaelbuckingham22@gmail.com>
2025-04-18 17:01:25 -0400

    Settings page, other small changes

---
+++ src/scripts/settings.js
@@ -0,0 +1,15 @@
+const currencies_url = 'https://api.frankfurter.dev/v1/currencies';
+
+// set up currency dropdown
+fetch(currencies_url)
+.then(response => response.json())
+.then(currencies => {
+    let currency_select = document.getElementById('currency-select');
+    for (currency_code in currencies) {
+        let new_option = document.createElement('option');
+        new_option.value = currency_code;
+        new_option.textContent = `${currencies[currency_code]} (${currency_code})`;
+        currency_select.appendChild(new_option);
+    }
+})
+.catch(err => console.log(err));
